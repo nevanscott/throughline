03751b88b8f2e302148ba8d823b3bd4ba7b45a4e
Nevan Scott <nfscott@gmail.com>
2021-07-26 23:54:50 +0200

    refactor fs promises

--- db/index.js
+++ db/index.js
@@ -1,18 +1,18 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const file = path.join(__dirname, './messages');
 
 async function writeEntry(entry) {
-  const text = await fs.promises.readFile(file).catch(error => console.error(error));
+  const text = await fs.readFile(file).catch(error => console.error(error));
   const data = text ? text.toString().trim().split('\n') : [];
   data.push(entry);
   const updated = data.join('\n') + '\n';
-  await fs.promises.writeFile(file, updated).catch(error => console.error(error));
+  await fs.writeFile(file, updated).catch(error => console.error(error));
 }
 
 async function getEntries() {
-  const text = await fs.promises.readFile(file).catch(error => console.error(error));
+  const text = await fs.readFile(file).catch(error => console.error(error));
   const data = text ? text.toString().trim().split('\n') : [];
   return data;
 }
