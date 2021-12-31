29fa10e33750a3d4bf90fa5397ba55d2e520ec11
Nevan Scott <nfscott@gmail.com>
2021-12-31 01:22:43 +0100

    change placement of logging

--- server/router.js
+++ server/router.js
@@ -16,8 +16,8 @@ async function serveStaticFile(filename, response) {
 }
 
 async function handleRequest(request, response) {
-  const { pathname } = new URL(request.url, `http://${request.headers.host}`);
   console.log(`Router: Received ${request.method} request for ${request.url}`);
+  const { pathname } = new URL(request.url, `http://${request.headers.host}`);
   if (request.method === 'GET' && pathname === '/') {
     console.log(`Router: Responding with file client/index.html`);
     await serveStaticFile('../client/index.html', response);
@@ -28,8 +28,8 @@ async function handleRequest(request, response) {
     console.log(`Router: Responding with file client/main.js`);
     await serveStaticFile('../client/main.js', response);
   } else if (request.method === 'GET' && pathname === '/api/messages') {
-    const data = await store.getEntries();
     console.log(`Router: Responding with list of messages`);
+    const data = await store.getEntries();
     response.end(JSON.stringify(data));
   } else if (request.method === 'POST' && pathname === '/api/post') {
     let data = '';
